1604410449c92e7c016a10326d3bd12e84a7a181
Vidhyanand CS <vithushaji@gmail.com>
2020-09-09 22:12:13 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const axios = require('axios')
+const hbs = require('hbs')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req,res) => {
+
+    res.render('index')
+
+})
+
+app.get('/pattern', (req,res) => {
+
+    res.render('pattern')
+
+})
+
+app.listen(3000, () => {
+    console.log('server is up and running in port 3000')
+})
